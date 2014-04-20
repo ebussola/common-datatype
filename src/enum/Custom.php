68b042db90d5dd54a726783fa135ab3b7fdc6cc1
Leonardo Shinagawa <leonardo@ebussola.com>
2014-04-20 13:11:16 -0300

    changed autoload to psr-4. Added ArrayList and Custom Enum

---
+++ src/enum/Custom.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ebussola\common\datatype\enum;
+
+use ebussola\common\datatype\Enum;
+
+
+final class Custom extends Enum {
+
+    /**
+     * @var array
+     */
+    private $_default;
+
+    public function __construct(array $values, $value='UNDEFINED') {
+        $this->_default = $values;
+
+        parent::__construct($value);
+    }
+
+    /**
+     * @return array
+     * Return a list of available options.
+     */
+    public function defaults() {
+        return $this->_default;
+    }
+
+    /**
+     * @return String
+     * Return the name identifying the Enum class
+     */
+    public function enumId() {
+        return __CLASS__;
+    }
+
+}
---
+++ src/enum/Sort.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ebussola\common\datatype\enum;
+
+use ebussola\common\datatype\Enum;
+
+
+class Sort extends Enum
+{
+
+    /**
+     * @return array
+     * Return a list of options available.
+     */
+    public function defaults() {
+        return array(
+            1 => 'ASC',
+            2 => 'DESC'
+        );
+    }
+
+    /**
+     * @return String
+     * Return the name identifying the Enum class
+     */
+    public function enumId() {
+        return __CLASS__;
+    }
+
+}
