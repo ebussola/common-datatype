138cad452581eb3d2c9678c2dcbca5dce14a5833
Leonardo Shinagawa <leonardo@ebussola.com>
2014-04-21 12:07:22 -0300

    some tests to ArrayList. Changed all the methods to camelcase

--- src/ArrayList.php
+++ src/ArrayList.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace ebussola\common\datatype;
+use Doctrine\Common\Inflector\Inflector;
 
 /**
  * Thanks to Miguel Elias Santos <migueleliasweb at gmail dot com>
@@ -8,28 +9,28 @@ namespace ebussola\common\datatype;
  * Alternative implementation of PHP's native
  * It magically (__call) routes all 'array_' native function correnctly
  *
- * @method ArrayList change_key_case    Changes all keys in an array
+ * @method ArrayList changeKeyCase    Changes all keys in an array
  * @method ArrayList chunk              Split an array into chunks
  * @method ArrayList combine            Creates an array by using one array for keys and another for its values
- * @method ArrayList count_values       Counts all the values of an array
- * @method ArrayList diff_assoc         Computes the difference of arrays with additional index check
- * @method ArrayList diff_key           Computes the difference of arrays using keys for comparison
- * @method ArrayList diff_uassoc        Computes the difference of arrays with additional index check which is performed by a user supplied callback function
- * @method ArrayList diff_ukey          Computes the difference of arrays using a callback function on the keys for comparison
+ * @method ArrayList countValues       Counts all the values of an array
+ * @method ArrayList diffAssoc         Computes the difference of arrays with additional index check
+ * @method ArrayList diffKey           Computes the difference of arrays using keys for comparison
+ * @method ArrayList diffUassoc        Computes the difference of arrays with additional index check which is performed by a user supplied callback function
+ * @method ArrayList diffUkey          Computes the difference of arrays using a callback function on the keys for comparison
  * @method ArrayList diff               Computes the difference of arrays
- * @method ArrayList fill_keys          Fill an array with values, specifying keys
+ * @method ArrayList fillKeys          Fill an array with values, specifying keys
  * @method ArrayList fill               Fill an array with values
  * @method ArrayList filter             Filters elements of an array using a callback function
  * @method ArrayList flip               Exchanges all keys with their associated values in an array
- * @method ArrayList intersect_assoc    Computes the intersection of arrays with additional index check
- * @method ArrayList intersect_key      Computes the intersection of arrays using keys for comparison
- * @method ArrayList intersect_uassoc   Computes the intersection of arrays with additional index check, compares indexes by a callback function
- * @method ArrayList intersect_ukey     Computes the intersection of arrays using a callback function on the keys for comparison
+ * @method ArrayList intersectAssoc    Computes the intersection of arrays with additional index check
+ * @method ArrayList intersectKey      Computes the intersection of arrays using keys for comparison
+ * @method ArrayList intersectUassoc   Computes the intersection of arrays with additional index check, compares indexes by a callback function
+ * @method ArrayList intersectUkey     Computes the intersection of arrays using a callback function on the keys for comparison
  * @method ArrayList intersect          Computes the intersection of arrays
- * @method ArrayList key_exists         Checks if the given key or index exists in the array
+ * @method ArrayList keyExists         Checks if the given key or index exists in the array
  * @method ArrayList keys               Return all the keys or a subset of the keys of an array
  * @method ArrayList map                Applies the callback to the elements of the given arrays
- * @method ArrayList merge_recursive    Merge two or more arrays recursively
+ * @method ArrayList mergeRecursive    Merge two or more arrays recursively
  * @method ArrayList merge              Merge one or more arrays
  * @method ArrayList multisort          Sort multiple or multi-dimensional arrays
  * @method ArrayList pad                Pad array to the specified length with a value
@@ -38,7 +39,7 @@ namespace ebussola\common\datatype;
  * @method ArrayList push               Push one or more elements onto the end of array
  * @method ArrayList rand               Pick one or more random entries out of an array
  * @method ArrayList reduce             Iteratively reduce the array to a single value using a callback function
- * @method ArrayList replace_recursive  Replaces elements from passed arrays into the first array recursively
+ * @method ArrayList replaceRecursive  Replaces elements from passed arrays into the first array recursively
  * @method ArrayList replace            Replaces elements from passed arrays into the first array
  * @method ArrayList reverse            Return an array with elements in reverse order
  * @method ArrayList search             Searches the array for a given value and returns the corresponding key if successful
@@ -46,21 +47,21 @@ namespace ebussola\common\datatype;
  * @method ArrayList slice              Extract a slice of the array
  * @method ArrayList splice             Remove a portion of the array and replace it with something else
  * @method ArrayList sum                Calculate the sum of values in an array
- * @method ArrayList udiff_assoc        Computes the difference of arrays with additional index check, compares data by a callback function
- * @method ArrayList udiff_uassoc       Computes the difference of arrays with additional index check, compares data and indexes by a callback function
+ * @method ArrayList udiffAssoc        Computes the difference of arrays with additional index check, compares data by a callback function
+ * @method ArrayList udiffUassoc       Computes the difference of arrays with additional index check, compares data and indexes by a callback function
  * @method ArrayList udiff              Computes the difference of arrays by using a callback function for data comparison
- * @method ArrayList uintersect_assoc   Computes the intersection of arrays with additional index check, compares data by a callback function
- * @method ArrayList uintersect_uassoc  Computes the intersection of arrays with additional index check, compares data and indexes by a callback functions
+ * @method ArrayList uintersectAssoc   Computes the intersection of arrays with additional index check, compares data by a callback function
+ * @method ArrayList uintersectUassoc  Computes the intersection of arrays with additional index check, compares data and indexes by a callback functions
  * @method ArrayList uintersect         Computes the intersection of arrays, compares data by a callback function
  * @method ArrayList unique             Removes duplicate values from an array
  * @method ArrayList unshift            Prepend one or more elements to the beginning of an array
  * @method ArrayList values             Return all the values of an array
- * @method ArrayList walk_recursive     Apply a user function recursively to every member of an array
+ * @method ArrayList walkRecursive     Apply a user function recursively to every member of an array
  */
 class ArrayList extends \ArrayObject {
 
     public function __call($func, $argv) {
-        $func = 'array_'.$func;
+        $func = 'array_' . Inflector::tableize($func);
         if (!is_callable($func)) {
             throw new \BadMethodCallException(__CLASS__.'->'.$func);
         }
---
+++ test/ArrayListTest.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shina
+ * Date: 21/04/14
+ * Time: 11:36
+ */
+
+class ArrayListTest extends PHPUnit_Framework_TestCase {
+
+    public function testChangeKeyCase() {
+        $arr = new \ebussola\common\datatype\ArrayList();
+        $arr['Leo'] = 1;
+        $arr['LeoNardo'] = 1;
+        $arr['Shina'] = 1;
+
+        $arr = $arr->changeKeyCase(CASE_LOWER);
+
+        $a = 1;
+        foreach ($arr as $i => $val) {
+            switch ($a) {
+                case 1 :
+                    $this->assertEquals('leo', $i);
+                    break;
+                case 2 :
+                    $this->assertEquals('leonardo', $i);
+                    break;
+                case 3 :
+                    $this->assertEquals('shina', $i);
+                    break;
+            }
+
+            $a++;
+        }
+
+    }
+
+    public function testChunk() {
+        $arr = new \ebussola\common\datatype\ArrayList();
+        $arr[] = 1;
+        $arr[] = 2;
+        $arr[] = 3;
+        $arr[] = 4;
+        $arr[] = 5;
+        $arr[] = 6;
+
+        $arr = $arr->chunk(2);
+
+        foreach ($arr as $_arr) {
+            $this->assertCount(2, $_arr);
+        }
+    }
+
+    public function testMap() {
+        $arr = new \ebussola\common\datatype\ArrayList([1, 2, 3, 4, 5, 6]);
+
+        $arr = $arr->map(function($item) {
+            return $item+1;
+        });
+
+        foreach ($arr as $i => $val) {
+            switch ($i) {
+                case 0 :
+                    $this->assertEquals(2, $val);
+                    break;
+                case 1 :
+                    $this->assertEquals(3, $val);
+                    break;
+                case 2 :
+                    $this->assertEquals(4, $val);
+                    break;
+                case 3 :
+                    $this->assertEquals(5, $val);
+                    break;
+                case 4 :
+                    $this->assertEquals(6, $val);
+                    break;
+                case 5 :
+                    $this->assertEquals(7, $val);
+                    break;
+            }
+        }
+    }
+
+    public function testSearch() {
+        $arr = new \ebussola\common\datatype\ArrayList([1, 2, 3, 4, 5]);
+        $index = $arr->search(3);
+
+        $this->assertEquals($index, 2);
+    }
+
+}
