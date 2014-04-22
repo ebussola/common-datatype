3eaa73f20af356b07dfdfcf1c2dc1e8351c14fdd
Leonardo Shinagawa <leonardo@ebussola.com>
2014-04-21 22:55:49 -0300

    Percentage tests

---
+++ test/PercentageTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shina
+ * Date: 21/04/14
+ * Time: 22:49
+ */
+
+class PercentageTest extends PHPUnit_Framework_TestCase {
+
+    public function testGeneralUse() {
+        $percent = new \ebussola\common\datatype\number\Percentage(10);
+        $this->assertEquals('10,00%', (string)$percent);
+
+        $percent->setShowSymbol(false);
+        $this->assertEquals('10,00', (string)$percent);
+
+        $number = $percent->of(100);
+        $this->assertEquals(10, $number->getValue());
+        $this->assertInstanceOf('\ebussola\common\datatype\Number', $number);
+
+        $currency = $percent->of(new \ebussola\common\datatype\number\Currency(100));
+        $this->assertEquals(10, $currency->getValue());
+        $this->assertInstanceOf('\ebussola\common\datatype\number\Currency', $currency);
+    }
+
+}
